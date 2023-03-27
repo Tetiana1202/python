0c277e4d60853814c5a9d1af640ec8d08c57f602
Tetiana1202 <tetiana.12.02.1999@gmail.com>
2023-03-27 18:53:15 +0300

    visit to restaurant

---
+++ Restaurant.py
@@ -0,0 +1,24 @@
+import random
+
+# Запитати в користувача список страв
+foods = input("Введіть список страв, розділених комами: ").split(",")
+
+# Створити порожній словник для зберігання цін на страви
+prices = {}
+
+# Заповнити словник цінами на страви
+for food in foods:
+    prices[food.strip()] = round(random.uniform(5, 20), 2)
+
+# Вивести чек із назвами страв та цінами
+print("Кафе у Тетяни")
+print("===")
+
+# Порахувати загальну суму
+sum = 0
+for food, price in prices.items():
+    print(food + ": " + str(price) + " грн")
+    sum += price
+
+print("Всього: " + str(sum) + " грн")
+print("===")
