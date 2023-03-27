88cf8f891d75ad1bd2eef5072e7890f57bbd8ef3
Tetiana1202 <tetiana.12.02.1999@gmail.com>
2023-03-27 19:09:11 +0300

    refactored

--- SlicingProject.py
+++ SlicingProject.py
@@ -2,9 +2,18 @@ property_transfer_xml = """<con:name>AuthTocken</con:name><con:sourceType>Respon
 //ns1:loginResponse/loginReturn[1]</con:sourcePath><con:targetType>Request</con:targetType><con:targetStep>multiCall</con:targetStep><con:targetPath>declare namespace urn='urn:Magento'; 
 //urn:multiCall/sessionId['?']</con:targetPath>""" 
  
+print(property_transfer_xml)
 start_pos = property_transfer_xml.find("<con:targetType>") 
+print("Start pos:", start_pos)
 end_pos = property_transfer_xml.find("</con:targetType>") 
+print("End pos:", end_pos)
  
-result = property_transfer_xml[start_pos:end_pos].split(">")[1].strip() 
+sliced = property_transfer_xml[start_pos:end_pos]
+print("After slicing:", sliced)
+
+splitted = sliced.split(">")[1]
+print("After split:", splitted)
+
+result = splitted.strip() 
  
 print("The value of con:targetType attribute is:", result)
